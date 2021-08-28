3b0ce7c91ac34a7ce6ca825c96daad3f9cc5a7a1
Tigana-Sari <tigana.sari@gmail.com>
2021-08-28 20:11:14 +0100

    Computer options js

--- app.js
+++ app.js
@@ -15,10 +15,27 @@ const game = () => {
     };
 
     //play match
-    
+    const playMatch = () => {
+        const options = document.querySelectorAll(".options button");
+        const playerHand = document.querySelector(".player-hand");
+        const computerHand = document.querySelector(".computer-hand");
+
+        //computer options
+        const computerOptions = ['rock', 'paper', 'scissors'];
+        options.forEach(options=> {
+            options.addEventListener("click", function(){
+                //computer choice
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+                console.log(computerChoice);
+            }); 
+        });
+        const computerNumber = Math.floor(Math.random() * 3);
+        computerOptions[computerNumber];
+    }
 
     startGame();
-
+    playMatch();
 };
 
 game();
